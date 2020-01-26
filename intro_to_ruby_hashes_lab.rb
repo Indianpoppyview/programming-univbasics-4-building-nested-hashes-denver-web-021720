47c97c94b616c54bfa7ff84763cb3156c325167a
David Montoya <davidcmontoya@yahoo.com>
2020-01-26 20:42:21 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,7 +25,10 @@ def monopoly_with_third_tier
         shortline: {}
       },
       rent_in_dollars: {
-        
+        one_piece_owned:
+        two_pieces_owned:
+        three_pieces_owned:
+        four_pieces_owned:
       }
   }
 }   
